bd458639e37065c229aa86c61eed13591ef84f3c
Ari Epstein <ate2@cornell.edu>
2011-04-02 11:15:55 -0400

    Use factories for docket model specs.

--- vendor/engines/dockets/spec/models/docket_spec.rb
+++ vendor/engines/dockets/spec/models/docket_spec.rb
@@ -2,31 +2,23 @@ require 'spec_helper'
 
 describe Docket do
 
-  def reset_docket(options = {})
-    @valid_attributes = {
-      :id => 1,
-      :name => "RSpec is great for testing too"
-    }
-
-    @docket.destroy! if @docket
-    @docket = Docket.create!(@valid_attributes.update(options))
-  end
-
   before(:each) do
-    reset_docket
+    @docket = Factory(:docket)
   end
 
   context "validations" do
-    
-    it "rejects empty name" do
-      Docket.new(@valid_attributes.merge(:name => "")).should_not be_valid
+
+    it 'should not save without a name' do
+      @docket.name = nil
+      @docket.save.should be_false
     end
 
-    it "rejects non unique name" do
-      # as one gets created before each spec by reset_docket
-      Docket.new(@valid_attributes).should_not be_valid
+    it 'should not save with a duplicate name' do
+      duplicate = Factory.build(:docket, :name => @docket.name)
+      duplicate.save.should be_false
     end
-    
+
   end
 
 end
+
