d95690ccec839e54c1ac48af9ad6b0757d990a77
Ari Epstein <ate2@cornell.edu>
2011-04-04 10:11:07 -0400

    Fix some locales and controller issues to make things work with new organization.

--- vendor/engines/docketing/app/controllers/admin/docketing/dockets_controller.rb
+++ vendor/engines/docketing/app/controllers/admin/docketing/dockets_controller.rb
@@ -3,6 +3,7 @@ module Admin
     class DocketsController < Admin::BaseController
 
       crudify :docket, :searchable => true,
+              :redirect_to_url => :admin_docketing_dockets_url,
               :title_attribute => 'name', :xhr_paging => true
 
     end
