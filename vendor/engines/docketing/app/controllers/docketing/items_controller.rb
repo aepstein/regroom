62470d8db8d1dd23b6055f6522895e835532398f
Ari Epstein <ate2@cornell.edu>
2011-04-06 13:49:17 -0400

    Stubbed out actions for docketing item management by logged in users.

--- vendor/engines/docketing/app/controllers/docketing/items_controller.rb
+++ vendor/engines/docketing/app/controllers/docketing/items_controller.rb
@@ -2,6 +2,8 @@ module Docketing
   class ItemsController < DocketingController
 
     before_filter :find_all_docketing_items
+    before_filter :new_docketing_item_from_params, :only => [ :new, :create ]
+    before_filter :find_docketing_item, :only => [ :show, :edit, :update ]
 
     def index
       # you can use meta fields from your model instead (e.g. browser_title)
@@ -10,14 +12,39 @@ module Docketing
     end
 
     def show
-      @docketing_item = DocketingItem.find(params[:id])
 
       # you can use meta fields from your model instead (e.g. browser_title)
       # by swapping @page for @docket_item in the line below:
       present(@page)
     end
 
+    # POST /docketing/dockets/:docketing_docket_id/items
+    # POST /docketing/dockets/:docketing_docket_id/items.xml
+    def create
+    end
+
+    # GET /docketing/dockets/:docketing_docket_id/items/new
+    def new
+    end
+
+    # GET /docketing/items/:id/edit
+    def edit
+    end
+
+    # PUT /docketing/items/:id
+    # PUT /docketing/items/:id.xml
+    def update
+    end
+
   protected
+
+    def new_docketing_item_from_params
+      @docketing_item = @docketing_docket.items.build( params[:docketing_item] )
+    end
+
+    def find_docketing_item
+      @docketing_item = DocketingItem.find( params[:id] )
+    end
 
     def find_all_docketing_items
       @docketing_items = DocketingItem.scoped
